0c67552dcd18cf44326c4030f1e2af0ea70c0f93
SimonKazantsev <misemen4@gmail.com>
2025-11-28 11:19:42 +0500

    написал форму для заявки

--- tickets/schemas.py
+++ tickets/schemas.py
@@ -2,4 +2,23 @@ from django import forms
 
 
 class TaskForm(forms.Form):
-    status: 
+    status =  forms.CharField(
+        max_length = 30,
+        widget = forms.Select(
+            ('value1', 'value1'),
+            ('value2', 'value2'),
+            ('value3', 'value3'),
+            ('value4', 'value4'),
+        )
+    )
+    problem = forms.CharField()
+    priority = forms.CharField(
+        widget = forms.Select(
+            ('value1', 'value1'),
+            ('value2', 'value2'),
+            ('value3', 'value3'),
+            ('value4', 'value4'),
+        )
+    ),
+
+
