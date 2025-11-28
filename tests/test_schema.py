8fb43eb2248d052fd7fba2d3d8eca62b02c69878
SimonKazantsev <misemen4@gmail.com>
2025-11-28 16:07:02 +0500

    Написал форму Feedback, написал форму SendEmail для отправки текста, добавил files в форму Task

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from tickets.forms import TaskForm
+from tickets.forms import TaskForm, SendEmailForm, FeedbackForm
 from tickets.models import CategoryType
 
 class TaskFormTestCase(TestCase):
@@ -12,16 +12,55 @@ class TaskFormTestCase(TestCase):
             "status": 'waiting',
             "problem": "Base problem",
             "priority": 'value1',
-            "category": self.category
+            "category": self.category,
+            "files":  None
         }
-        task = TaskForm(data = valid_data)
-        self.assertTrue(task.is_valid())
+        task_form = TaskForm(data = valid_data)
+        self.assertTrue(task_form.is_valid())
     
     def test_from_invalid_data(self):
         invalid_data = {
         "status": 'invalid_status',
         "problem": "Same problem",
-        "priority": 'invalid_value'
+        "priority": 'invalid_value',
+        "category": self.category,
+        "files":  None
     }
-        task = TaskForm(data = invalid_data)
-        self.assertFalse(task.is_valid())
+        task_form = TaskForm(data = invalid_data)
+        self.assertFalse(task_form.is_valid())
+
+class SendEmailFornTestCase(TestCase):
+    def test_from_valid_data(self):
+        valid_data = {
+            'repicient': 'test@example.com',
+            'mail_theme': 'Test Theme',
+            'text': 'This is a test email.',
+            'files': None
+        }
+        mail_form = SendEmailForm(valid_data)
+        self.assertTrue(mail_form.is_valid())
+
+    def test_from_invalid_data(self):
+        invalid_data = {
+            "repicient": "invalid mail",
+            "mail_theme": "Theme for a Mail!"
+        }
+        mail_form = SendEmailForm(invalid_data)
+        self.assertFalse(mail_form.is_valid())
+
+class FeedbackFormTestCase(TestCase):
+    def test_from_valid_data(self):
+        valid_data = {
+            "rating": "1",
+            "feedback": "Good!"
+        }
+        feedback_form = FeedbackForm(valid_data)
+        self.assertTrue(feedback_form.is_valid())
+    
+    def test_from_invalid_data(self):
+        valid_data = {
+            "rating": "invalid_data",
+            "feedback": "Good!"
+        }
+        feedback_form = FeedbackForm(valid_data)
+        self.assertFalse(feedback_form.is_valid())
--- tickets/forms.py
+++ tickets/forms.py
@@ -1,12 +1,30 @@
+from django import forms
+from django.core.validators import EmailValidator
 from django.forms import ModelForm
 from tickets.models import Task
 
-class TaskForm(ModelForm):
+EMAIL_ADDRESS_MAX_LENGTH = 256
 
+class TaskForm(ModelForm):
+    files = forms.FileField(required = None)
     class Meta:
         model = Task
         fields = ["status", "problem", "priority", "title", "category"] 
 
 
-    
+class SendEmailForm(forms.Form):
+    repicient = forms.EmailField(
+        max_length = EMAIL_ADDRESS_MAX_LENGTH,
+        validators=[EmailValidator]
+    )
+    mail_theme = forms.CharField(
+        max_length = 100
+    )
+    text = forms.CharField()
+    files = forms.FileField(required = False)
+
+class FeedbackForm(forms.Form):
+    FEEDBACK_RATING = [(str(i), i) for i in range(1, 6)]
+    rating = forms.ChoiceField(choices=FEEDBACK_RATING)
+    feedback = forms.CharField()
 
