05488c82014f260fef8b8dec3c88417d4601968e
SimonKazantsev <misemen4@gmail.com>
2025-12-05 11:33:39 +0500

    тестовый view на поиск всех сотрудников

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from tickets.models import Employee
 
 # Create your views here.
+
+
+async def get_all_workers(request):
+    first_worker = await Employee.objects.afirst()
+    if first_worker:
+        html = '<html lang="en"><body>We got one</body></html'
+        return HttpResponse(html)
+    html = '<html lang="en"><body>No one</body></html'
+    return HttpResponse(html)
+    
