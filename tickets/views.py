8dbde70f74cba8f8a09e4b857231e629f6af09a0
SimonKazantsev <misemen4@gmail.com>
2025-12-05 13:50:01 +0500

    пробую выгрузить сотрудников

--- tickets/views.py
+++ tickets/views.py
@@ -5,11 +5,7 @@ from tickets.models import Employee
 # Create your views here.
 
 
-async def get_all_workers(request):
-    first_worker = await Employee.objects.afirst()
-    if first_worker:
-        html = '<html lang="en"><body>We got one</body></html'
-        return HttpResponse(html)
-    html = '<html lang="en"><body>No one</body></html'
-    return HttpResponse(html)
+def get_all_workers(request):
+    all_worker = Employee.objects.all()
+    return render(request, 'tickets/employee.html', {'all_worker': all_worker}) 
     
