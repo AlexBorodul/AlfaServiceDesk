c82648bda9fd3fc9c25bd0dce4725ee8ef0e5ae8
SimonKazantsev <misemen4@gmail.com>
2025-12-10 17:56:47 +0500

    сбегаю в другую ветку уточнить по поводу директорий

--- service_desk/urls.py
+++ service_desk/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import django.contrib.auth.views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", auth_views.LoginView.as_view(template_name = 'tickets/login.html'))
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+# def auth(request):
+#     if request.method == "GET":
+#         user = authenticate(username = request.data['login'], password = request.data['password'])
+#         if user:
+#             return render(request)
+#         raise ValueError
+#     else:
+#         form = 
