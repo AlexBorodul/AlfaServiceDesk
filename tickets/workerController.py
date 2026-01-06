c4915889ff7eac6557e683f3870063476d60f3f7
SimonKazantsev <misemen4@gmail.com>
2026-01-06 15:44:10 +0500

    докстринги, аннотация типов

--- tickets/workerController.py
+++ tickets/workerController.py
@@ -4,21 +4,21 @@ from tickets.models import Employee, Task
 class WorkerController:
     """Занимается выбором исполнителя задачи."""
     @classmethod
-    def auto_select_worker(cls, task: Task) -> Employee:
+    def auto_select_worker(cls, task: Task) -> None:
         """Автоматический выбор исполнителя."""
         workers = Employee.objects.filter(specialization = task.category, office=task.office).all()
         emptiest_worker = min(workers, key = WorkerController.get_worker_tasks)
         task.worker = emptiest_worker
-        return task
     @classmethod
-    def change_worker(self) -> Employee:
-        """Смена исполнителя"""
-        pass
+    def change_to_main_worker(cls, task: Task) -> None:
+        """Смена исполнителя на главного в офисе"""
+        task.worker = task.worker.parent
     @classmethod
-    def get_subordinates(self, main_worker) -> list[Employee]:
+    def get_subordinates(cls, main_worker: Employee) -> QuerySet[Employee]:
         """Возвращает список подчинённых"""
-        pass
+        return Employee.objects.filter(parent = main_worker)
 
     @classmethod
-    def get_worker_tasks(cls, worker: Employee):
+    def get_worker_tasks(cls, worker: Employee) -> int:
+        """Возвращает список заявок у исполнителя."""
         return len(Task.objects.filter(worker = worker))
